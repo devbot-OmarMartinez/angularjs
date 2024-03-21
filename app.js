c00c14e137bdbdd5d76166f5fb9b71590220b50f
Omar Martinez <fmartinez@devbot.com>
2024-03-21 10:47:51 -0600

    Change on branch weather-app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var weatherApp = angular.module('weatherApp', ['ngResource', 'ngRoute']);
 
-const apiKey = '037684aa48abd69f97cb8cf954402219';
+const apiKey = '';
 const apiUrl = `http://api.openweathermap.org/data/2.5/forecast`
 const apiWeatherUrl = `http://api.openweathermap.org/data/2.5/weather`
 
